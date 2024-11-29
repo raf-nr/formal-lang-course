4c6b5406b42b52bc72e7673c23828cac5f21dce0
raf-nr <nurmuhametovrafik04@gmail.com>
2024-11-29 19:46:59 +0300

    Fix imports in task9 tests

--- tests/autotests/test_task09.py
+++ tests/autotests/test_task09.py
@@ -15,10 +15,11 @@ from cfpq_concrete_cases import CaseCFPQ, CASES_CFPQ
 
 # Fix import statements in try block to run tests
 try:
-    from project.task6 import hellings_based_cfpq
-    from project.task7 import matrix_based_cfpq
-    from project.task8 import tensor_based_cfpq, cfg_to_rsm, ebnf_to_rsm
-    from project.task9 import gll_based_cfpq
+    from project.cfg.hellings_based_cfpq import hellings_based_cfpq
+    from project.cfg.matrix_based_cfpq import matrix_based_cfpq
+    from project.cfg.rsm_untils import cfg_to_rsm, ebnf_to_rsm
+    from project.cfg.tensor_based_cfpq import tensor_based_cfpq
+    from project.cfg.gll_based_cfpq import gll_based_cfpq
 except ImportError:
     pytestmark = pytest.mark.skip("Task 9 is not ready to test!")
 
